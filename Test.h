5a5a9169d5e7798175953272fe8623626d11dd50
Dominic Pellegrino <DJPellegrino@Dominics-MacBook-Air.local>
2015-10-05 15:40:56 -0700

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Dominic Pellegrino on 10/5/15.
+//  Copyright © 2015 Dominic Pellegrino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Dominic Pellegrino on 10/5/15.
+//  Copyright © 2015 Dominic Pellegrino. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
